e79a3fcb01aa1c4beea8a26fe5c3656c4386ba43
orionpax00 <durgesh123.iitr@gmail.com>
2019-08-09 01:44:18 +0530

    thanos snap

--- task_.cpp
+++ task_.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <algorithm> 
 
 using namespace std;
+main(){
+    
+    int input;
+    cout<<"please enter length of array: ";
+    cin>>input;
+    
+    int arr[input];
+    
+    for (int i = 0 ; i < input; ++i){
+        cin>>arr[i];
+    };
 
-int main(){
-    cout<<" hi" <<endl;
+    bool sort_ = std::is_sorted(arr, arr + input);
+
+    if(sort_){
+        cout<<"0";
+    }
+    else{
+        
+    }
 }
