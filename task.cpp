8a403fe1dd1465350a13de96257b6c9cf8a3a64d
orionpax00 <durgesh123.iitr@gmail.com>
2019-08-11 01:00:17 +0530

    Arabic and English

--- task.cpp
+++ task.cpp
@@ -1,49 +1,46 @@
 #include <iostream>
-#include <string.h>
-
-
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <string>
 using namespace std;
 
-int counter_(string n,  char a){
-    
-    int count = 0;
-    for (int i=0; i< n.length(); i++){
-        if (n[i] == a){
-            count++;
+int main()
+{
+    ios_base::sync_with_stdio(0) ;
+    int t , n , x = 0 ;
+    bool b = 0 ;
+    cin >> t ;
+    vector<string> vect ;
+    while(t--)
+    {
+        cin >> n ;
+        vect.resize(n) ;
+        for(int i=0;i<n;i++)
+        {
+            cin >> vect[i] ;
+            if(vect[i][0]>='a')
+            {
+                b = 1 ;
+                x = i ;
+            }
         }
-    }
-
-    return count;
-}
-
-int main(){
-    int input;
-    cin>>input;
-    int counter = 0;
-    while( counter < input){
-        counter += 1;
-
-        string n;
-
-        cin>>n;
-        int value_ = 0;
-        char num_ = n[0];
-        for (int i=0; i< n.length(); i++){
-            int count = counter_(n, n[i]);
-            if(count > value_){
-                value_ = count;
-                num_ = n[i];
-            } 
-            else if (count == value_){
-                if(n[i]<num_){
-                    num_ = n[i];
-                }
-            }       
+        if(b)
+        {
+            for(int i=x+1;i<n;i++)
+                cout << vect[i] << " " ;
+            cout << vect[x] << " " ;
+            for(int i=0;i<x;i++)
+                cout << vect[i] << " " ;
+            cout << endl ;
+        }else
+        {
+            for(int i=0;i<n;i++)
+                cout << vect[i] << " " ;
+            cout << endl ;
         }
-    
-        cout<< value_  <<" " << num_<< "\n";
-
+        b = 0 ;
     }
-
     return 0;
 }
