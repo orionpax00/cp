84d643516c53963d4cffac7864f7e606c12fa874
orionpax00 <durgesh123.iitr@gmail.com>
2019-08-10 18:02:32 +0530

    ladder 1

--- task.cpp
+++ task.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main(){
+    int input;
+    int arr[input][2];
+    cin>>input;
+    for (int i = 0; i<input; ++i){
+            cin>>arr[i][0];
+            cin>>arr[i][1];
+        }
+    for (int i = 0; i<input; ++i){
+            cout<<arr[i][0]+arr[i][1]<<endl;
+        }
     
-}
+    }
