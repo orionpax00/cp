a22fa434b41889f699a2c70ba94fcfb8347a54ca
orionpax00 <durgesh123.iitr@gmail.com>
2019-08-11 00:14:23 +0530

    Omar Loves Candies, Again

--- task.cpp
+++ task.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string.h>
+
 
 using namespace std;
+
+int counter_(string n,  char a){
+    
+    int count = 0;
+    for (int i=0; i< n.length(); i++){
+        if (n[i] == a){
+            count++;
+        }
+    }
+
+    return count;
+}
 
 int main(){
     int input;
@@ -8,27 +22,28 @@ int main(){
     int counter = 0;
     while( counter < input){
         counter += 1;
-        int n;
-        int x;
-        int y;
+
+        string n;
+
         cin>>n;
-        cin>>x;
-        cin>>y;
-        int arr[n];
-        for( int i = 0; i < n; ++i){
-            cin>>arr[i];
+        int value_ = 0;
+        char num_ = n[0];
+        for (int i=0; i< n.length(); i++){
+            int count = counter_(n, n[i]);
+            if(count > value_){
+                value_ = count;
+                num_ = n[i];
+            } 
+            else if (count == value_){
+                if(n[i]<num_){
+                    num_ = n[i];
+                }
+            }       
         }
-        if (arr[0] == x && arr[n-1] == y){
-            cout<<"BOTH"<<endl;
-        }
-        else if (arr[0] == x && arr[n-1] != y){
-            cout<<"EASY"<<endl;
-        }
-        else if (arr[0] != x && arr[n-1] == y){
-            cout<<"HARD"<<endl;
-        }
-        else if(arr[0] != x && arr[n-1] != y){
-            cout<<"OKAY"<<endl;
-        }
+    
+        cout<< value_  <<" " << num_<< "\n";
+
     }
+
+    return 0;
 }
