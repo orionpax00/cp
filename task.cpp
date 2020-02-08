b1119c87ddb2a440ddb9fcde5c7c2cfbf3eba99a
orionpax00 <durgesh123.iitr@gmail.com>
2020-02-08 15:40:48 +0530

    codechef feb challenge

--- task.cpp
+++ task.cpp
@@ -1,18 +1,29 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
- ( ){
-  
-}
-
-int main(){
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-
-  freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-  
-  return 0;
+int main() {
+	int t;
+  cin>>t;
+	while(t--){
+	  int n;
+	  cin>>n;
+	  int arr1[n];
+	  int arr2[n];
+	  for(int i=0;i<n;++i){
+	    cin>>arr1[i];
+	  }
+	  for(int i=0;i<n;++i){
+	    cin>>arr2[i];
+	  }
+	  
+	  sort(arr1, arr1+n);
+	  sort(arr2, arr2+n);
+	  long long int ans =0;
+	  for(int i=0;i<n;++i){
+	   ans += min(arr1[i],arr2[i]);
+	  }
+	  
+	  cout<<ans<<"\n";
+	}
+	return 0;
 }
