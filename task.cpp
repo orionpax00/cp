7e086234dcb653c78f8a0c3aadd9f9c566eccb12
orionpax00 <durgesh123.iitr@gmail.com>
2020-02-12 18:39:11 +0530

    builin_clz function

--- task.cpp
+++ task.cpp
@@ -1,29 +1,26 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-	int t;
-  cin>>t;
-	while(t--){
-	  int n;
-	  cin>>n;
-	  int arr1[n];
-	  int arr2[n];
-	  for(int i=0;i<n;++i){
-	    cin>>arr1[i];
-	  }
-	  for(int i=0;i<n;++i){
-	    cin>>arr2[i];
-	  }
-	  
-	  sort(arr1, arr1+n);
-	  sort(arr2, arr2+n);
-	  long long int ans =0;
-	  for(int i=0;i<n;++i){
-	   ans += min(arr1[i],arr2[i]);
-	  }
-	  
-	  cout<<ans<<"\n";
+vector<int> findfactors(int n){
+	vector<int> factors;
+	for(int i=1;i<sqrt(n);i++){
+		if(n%i==0){
+			if(n/i==i) factors.push_back(i);
+			else {factors.push_back(i);factors.push_back(n/i);}
+		}
 	}
+	return factors;
+}			
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int x=17;
+	int ans = 8*sizeof(int) - __builtin_clz(x);
+	cout<<pow(2,ans);
+	
 	return 0;
 }
