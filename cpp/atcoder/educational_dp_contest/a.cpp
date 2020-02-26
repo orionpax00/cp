669b075e62afad4d1a42124534220e0bc55b3e8b
Durgesh <durgesh123.iitr@gmail.com>
2020-02-27 00:26:09 +0530

    probl1m a solved

--- cpp/atcoder/educational_dp_contest/a.cpp
+++ cpp/atcoder/educational_dp_contest/a.cpp
@@ -1,13 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
   freopen("input.txt", "r", stdin);
   freopen("output.txt", "w", stdout);
- 
+  const int INF = 1e9 + 5;
+  
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int& x: arr){
+    cin>>x;
+  };
+  vector<int> dp(n,INF);
+  dp[0] = 0;
+  for(int i=0;i<n;i++){
+    for(int j :{i+1, i+2}){
+      if(j<n){
+        dp[j] = min(dp[j], dp[i]+abs(arr[i] -  arr[j]));
+      }
+    }
+  }
+  cout<<dp[n-1];
   return 0;
 }
-
