6478681787cbc5eb08b4c9dc87890e63c33249ac
Durgesh <durgesh123.iitr@gmail.com>
2020-04-13 21:21:02 +0530

    hr interview questions

--- practice/atcoder/educational_dp_contest/d.cpp
+++ practice/atcoder/educational_dp_contest/d.cpp
@@ -6,11 +6,13 @@ int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
 
   int n, w;cin >> n >> w;
   int arr[n+1][2];
+  arr[0][0] = 0;
+  arr[0][1] = 0;
 
   for(int i =0 ; i< n ; i++){
     cin>>arr[i+1][0] >> arr[i+1][1]; 
@@ -21,7 +23,7 @@ int main(){
     for(int j=1;j<w+1; j++){
       if(j-arr[i][0]>=0){
         dp[i][j] = max(dp[i-1][j], dp[i-1][j-arr[i][0]] + arr[i][1]);
-      } 
+      }
       else
         dp[i][j] = dp[i-1][j]; 
     }
