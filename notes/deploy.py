6aa690ad0a8be66693f2dea1382ab35283655a24
Durgesh <durgesh123.iitr@gmail.com>
2020-08-01 13:26:16 +0530

    New changes

--- notes/deploy.py
+++ notes/deploy.py
@@ -2,12 +2,13 @@ import os
 
 CLOUD = os.environ["CLOUD"]
 
-with open('./index.md', 'w') as IndexF:
-    for filename in os.listdir("."):
-        if filename[-4:] == "html" :
-            IndexF.write("* [{}](./{})\n".format(filename.replace("_", " ")[:-5].title(), filename))
-
 if CLOUD:
     for filename in os.listdir("."):
         if filename[-2:] == "md":
             os.system("rm ./{}".format(filename))
+
+
+with open('./index.md', 'w') as IndexF:
+    for filename in os.listdir("."):
+        if filename[-4:] == "html" :
+            IndexF.write("* [{}](./{})\n".format(filename.replace("_", " ")[:-5].title(), filename))
