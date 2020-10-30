160877824a6891714e9efe5dd3f47da9bcaad06f
Durgesh <durgesh123.iitr@gmail.com>
2020-10-30 20:00:55 +0530

    new templates adde

---
+++ letsolve/hackerearth/smallest-dividable-segment.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+	int n, q, k; cin >> q;
+	vector<int	
+}
+
+int32_t main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+	int tt = 1; 
+	cin >> tt;
+	while(tt--) solve();
+
+
+	#ifdef LOCAL
+	cerr << "\n";
+	#endif
+	return 0;
+}
